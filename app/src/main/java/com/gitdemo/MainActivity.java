d1f5f7c70832f8a2de0101df4fcd9d6b500cebfb
zhongquan <zhongquan6554@163.com>
2016-11-04 10:05:22 +0800

    1、首次提交

--- app/src/main/java/com/gitdemo/MainActivity.java
+++ app/src/main/java/com/gitdemo/MainActivity.java
@@ -2,8 +2,13 @@ package com.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
+    String
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void initView() {
+    public static MainActivity getInstance() {
+        return this;
+    }
+
+    private void initView(String name) {
+        System.err.println("");
+        System.out.printf("");
+        System.out.println();
+        System.out.println("MainActivity.initView");
+        System.out.println("name = [" + name + "]");
+        System.out.println("name = " + name);
 
     }
 }
